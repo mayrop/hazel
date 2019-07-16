a23c916a8afb490e72e12ab297a2ac49818725fe
Mayra Valdes <mayra.valdes@justia.com>
2019-07-17 00:29:27 +0100

    My first function

---
+++ R/vectors.R
@@ -0,0 +1,11 @@
+#' Compute Vector Diff
+#'
+#' This function computes the difference of arrays
+#'
+#' @param array1 Vector to compare from
+#' @param array2 Vector to compare against
+#' @return Vector containing all the items from vector1 that are not part of vector2
+#' @export
+vector_diff <- function(vector1, vector2) {
+  return(vector1[!(vector1 %in% vector2)])
+}
